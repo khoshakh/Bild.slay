59d6b9eff7f8238b4c29d3361a5932f7552324dc
khoshakh <128104491+khoshakh@users.noreply.github.com>
2023-03-19 14:38:19 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+"""A collection of function for doing my project."""
+
+### We can put our words list in here as well as the images for hangman
+
+def my_func():
+    pass
+
+def my_other_func():
+    pass
