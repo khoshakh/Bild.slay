3db36dd1ab84c0d8178e7ef367fb7fc88227d3b3
khoshakh <128104491+khoshakh@users.noreply.github.com>
2023-03-19 15:30:03 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,13 @@
 
 ### We can put our words list in here as well as the images for hangman
 
-words= [‘allele’, ‘adaptation’, ‘anatomy’, ‘anaphase’, ‘anticodon’, ‘apoptosis’, ‘autotroph’, ‘bacteria’, ‘basepair’, ‘biodiversity’, ‘brain’, ‘cell’, ‘centromere’, ‘centrosome’, ‘chitin’, ‘chlorophyll’, ‘chloroplast’, ‘centriole’, ‘chromatid’, ‘competition’, ‘CRISPR’, ‘cytokinesis’, ‘cytoplasm’, ‘cytoskeleton’, ‘decomposition’, ‘diploid’, ‘disaccharide’, ‘DNA’, ‘evolution’, ‘enzyme’, ‘ecosystem’, ‘ectotherm’, ‘endotherm’, ‘epistasis’, ‘eukaryote’, ‘exon’, ‘exoskeleton’, ‘fungi’, ‘gamete’, ‘gene’, ‘genotype’, ‘genus’, ‘glycerol’, ‘glycolysis’, ‘haploid’, ‘heredity’, ‘heterozygous’, ‘homozygous’, ‘interphase’, ‘intron’, ‘lactase’, ‘ligand’, ‘metaphase’, ‘mitosis’, ‘meiosis’, ‘mRNA’, ‘nucleotide’, ‘organ’, ‘osmosis’, ‘organelle’, ‘phenotype’, ‘phospholipid’, ‘phytoplankton’, ‘prokaryote’, ‘prophase’, ‘pyruvate’, ‘receptor’, ‘RNA’, ‘synapsis’, ‘systole’, ‘telomere’, ‘thymine’, ‘transcription’, ‘transferrin’, ‘translation’, ‘vacuole’, ‘vesicle’, ‘virus’, ‘xylem’, ‘phloem’, ‘zooplankton’, ‘zygote’, ‘ribosome’, ‘mitochondria’]
+"""A collection of function for doing my project."""
+
+words = ['allele', 'adaptation', 'anatomy', 'anaphase', 'anticodon', 'apoptosis', 'autotroph', 'bacteria','basepair', 'biodiversity', 'brain', 'cell', 'centromere', 'centrosome', 'chitin', 'chlorophyll', 'chloroplast', 'centriole', 'chromatid', 'competition', 'CRISPR', 'cytokinesis', 'cytoplasm', 'cytoskeleton', 'decomposition','diploid']
+         
+##‘disaccharide’, ‘DNA’, ‘evolution’, ‘enzyme’, ‘ecosystem’, ‘ectotherm’, ‘endotherm’, ‘epistasis’, ‘eukaryote’, ‘exon’, ‘exoskeleton’, ‘fungi’, ‘gamete’, ‘gene’, ‘genotype’, ‘genus’, ‘glycerol’, ‘glycolysis’, ‘haploid’, ‘heredity’, ‘heterozygous’, ‘homozygous’, ‘interphase’, ‘intron’, ‘lactase’, ‘ligand’, ‘metaphase’, ‘mitosis’, ‘meiosis’, ‘mRNA’, ‘nucleotide’, ‘organ’, ‘osmosis’, ‘organelle’, ‘phenotype’, ‘phospholipid’, ‘phytoplankton’, ‘prokaryote’, ‘prophase’, ‘pyruvate’, ‘receptor’, ‘RNA’, ‘synapsis’, ‘systole’, ‘telomere’, ‘thymine’, ‘transcription’, ‘transferrin’, ‘translation’, ‘vacuole’, ‘vesicle’, ‘virus’, ‘xylem’, ‘phloem’, ‘zooplankton’, ‘zygote’, ‘ribosome’, ‘mitochondria’]
+
+import random  ##This module allows us down the line to randomly choose a word
 
 def get_word(words):
     word = random.choice(words)
