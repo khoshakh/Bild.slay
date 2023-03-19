40a1343ba214ddd0bb13fa254ca77569c14462c2
khoshakh <128104491+khoshakh@users.noreply.github.com>
2023-03-19 16:35:43 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,18 +14,49 @@ def get_word(words):
 
 '''The hangman function''' 
 
-def hangman():  ## Making the hangman game function 
+def hangman():  #Making the hangman game function 
     word = get_word(words)   ## word = choose_word(words)
     word_letters = set(word)  # letters in the word
     alphabet = set(string.ascii_uppercase)  ##Letters upper case to not confuse the computer
     used_letters = set()  # what the user has guessed ## We're setting it to be in a list
-    return used_letters
 
     cat_lives = 9  ## Cat hang man = 9 Lives (cat lives)
     
-    '''User input'''
+    while len(word_letters) >0 and lives >0: #While loop kicks in with 1st wrong guess
+        word_letters.append()
+        print("You have", remaining_lives, "lives left. You have used the letters:", used_letters,")
     
+    '''Creating the user input box and setting restrictions'''
     
+    user_letter = input('Guess a letter: ').upper() ##User prompt; capitalizes letter 
+    if user_letter in alphabet - used_letters:  #If guessed letter = new
+        used_letters.add(user_letter)  #Then added to used_letters
+    if user_letter in word_letters:   #If letter guessed = correct
+        word_letters.remove(user_letter)  #Then letter = removed from "word_letters"
+        print(user_letter)
+              
+    else:
+        remaining_lives = cat_lives-1
+        return remaining_lives
+        print("Your letter",user_letter,"is not in the word")
+    elif user_letterin used_letters:
+        print("You have already guessed that letter")
+              
+              
+  '''Error handling step below, ensures the guess is actually a letter'''
+              
+    else:   # We can make this more complex with is instance or type. etc. for grading
+        print("Must guess a letter. Invalid character detected.")
+              
+    if remaining_lives == 0:
+        print("Need to insert image from image function here") # Dislpay the whole hangman
+        print("You have died (×_x)... The word was:",word")
+    else: 
+        print("Good job! You guessed the word",word,"!")
+              
+def images():
+    
+        
     
 ##Need error handling
     ##Check if valid characters (only letters are used)
